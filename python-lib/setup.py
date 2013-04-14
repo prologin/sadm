c1fd0e211bda57242a8b38f95152390c1f100c06
Pierre Bourdon <delroth@gmail.com>
2013-04-14 12:11:42 +0200

    Add a setuptools/distribute installer script for libprologin

---
+++ python-lib/setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2013 Pierre Bourdon <pierre.bourdon@prologin.org>
+# Copyright (c) 2013 Association Prologin <info@prologin.org>
+#
+# Prologin-SADM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Prologin-SADM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Prologin-SADM.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(name='ProloginSADM',
+      version='1.0',
+      description='Utility libraries for Prologin Python services',
+      author='Pierre Bourdon',
+      author_email='pierre.bourdon@prologin.org',
+      url='http://bitbucket.org/prologin/sadm/',
+      packages=['prologin'],
+     )
