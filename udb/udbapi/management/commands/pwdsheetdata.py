11b72e00afaa3066a3f240cf3249fa6be9121524
Pierre Bourdon <delroth@gmail.com>
2013-04-24 08:51:51 +0200

    Added a manage.py pwdsheetdata command to udb for exporting realname/login/password in order to print a password sheet

---
+++ udb/udbapi/management/commands/pwdsheetdata.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2013 Pierre Bourdon <pierre.bourdon@prologin.org>
+# Copyright (c) 2013 Association Prologin <info@prologin.org>
+#
+# Prologin-SADM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Prologin-SADM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Prologin-SADM.  If not, see <http://www.gnu.org/licenses/>.
+
+# Exports the data required to print password sheets for users.
+
+from django.core.management import BaseCommand, CommandError
+from optparse import make_option
+from udbapi.models import User
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--type', default='user', help='User type (user/orga/root)'),
+    )
+
+    def handle(self, *args, **options):
+        users = User.objects.filter(group=options['type'])
+        for u in users:
+            print("%s\t%s\t%s" % (u.realname, u.login, u.password))
