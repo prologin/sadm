67230e47f59b036723c4f85d127260116434add7
Antoine Pietri <antoine.pietri@epita.fr>
2013-05-05 20:19:26 +0200

    refactorized notify, added weblog.py to log all announces

--- notify/weblog.py
+++ notify/weblog.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-import subprocess
 import notify_bot
 
 
 def callback(minutes, msg):
-    subprocess.call(
-        ['notify-send',
-         '--urgency', 'normal',
-         '--expire-time', str(minutes),
-         msg])
+    pass
 
 
 if __name__ == '__main__':
