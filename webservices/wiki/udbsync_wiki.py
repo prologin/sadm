99fd77b14e77ad8d89f30eef1bab66307a34b403
Antoine Pietri <antoine.pietri@epita.fr>
2013-05-06 10:45:09 +0200

    fixed udbsync_wiki

--- webservices/wiki/udbsync_wiki.py
+++ webservices/wiki/udbsync_wiki.py
@@ -11,7 +11,7 @@ env['PYTHONPATH'] = ('/var/prologin/wiki:' +
 
 def callback(users, updates_metadata):
     for u in users:
-        if updates_metadata[u] == 'created':
+        if updates_metadata[u.login] == 'created':
             subprocess.call([
                 'moin', 'account', 'create',
                 '--name', u.login,
@@ -20,18 +20,19 @@ def callback(users, updates_metadata):
                 '--password', u.password,
                 u.password,
             ], env=env)
-        elif updates_metadata[u] == 'deleted':
-            subprocess.call([
-                'moin', 'account', 'disable',
-                '--name', u.login,
-            ], env=env)
-        elif updates_metadata[u] == 'updated':
+        elif updates_metadata[u.login] == 'updated':
             subprocess.call([
                 'moin', 'account', 'resetpw',
                 '--name', u.login,
                 u.password,
             ], env=env)
-        
+    for login, status in updates_metadata.items():
+        if status == 'deleted':
+            subprocess.call([
+                'moin', 'account', 'disable',
+                '--name', login,
+            ], env=env)
+    
         
 c = prologin.udbsync.connect()
 c.poll_updates(callback, watch={'password'} or None)
