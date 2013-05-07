d40dd1ed7195480e118bd8711db6f04e0cc73d35
Antoine Pietri <antoine.pietri@epita.fr>
2013-05-07 09:22:46 +0200

    removed trailing space, s/w+/w/

--- udbsync/udbsync_rootssh.py
+++ udbsync/udbsync_rootssh.py
@@ -7,9 +7,9 @@ import prologin.udbsync
 
 def callback(users, updates_metadata):
     os.makedirs('/root/.ssh/', mode=0o700, exist_ok=True)
-    with open('/root/.ssh/authorized_keys', 'w+') as f:
+    with open('/root/.ssh/authorized_keys', 'w') as f:
         f.write('\n'.join(map(lambda x: x.ssh_key, users.values())))
 
-        
+
 c = prologin.udbsync.connect()
 c.poll_updates(callback, watch={'ssh_key'})
