66ae39cdd03d0fc02e96f6d9aa3357f2ae4ae091
Pierre-Marie de Rodat <pmderodat@kawie.fr>
2013-05-09 11:34:10 +0200

    presence*: always give a message on login refused

--- python-lib/prologin/presencesync.py
+++ python-lib/prologin/presencesync.py
@@ -57,7 +57,7 @@ class Client(prologin.synchronisation.Client):
             ' PresenceSync status code is {}'.format(r.status_code)
         )
         if r.status_code != 200:
-            return r.text
+            return r.text or 'No reason given'
         else:
             return None
 
