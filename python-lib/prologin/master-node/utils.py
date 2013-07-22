c08753ea5861098e60ead74f830ff75fa9b1d117
Antoine Pietri <antoine.pietri@epita.fr>
2013-07-23 00:09:23 +0200

    move psycopg wait() to use tornado's event loop

--- python-lib/prologin/master-node/utils.py
+++ python-lib/prologin/master-node/utils.py
@@ -17,27 +17,39 @@
 # You should have received a copy of the GNU General Public License
 # along with Stechec.  If not, see <http://www.gnu.org/licenses/>.
 
-import gevent.socket
 import psycopg2
 import psycopg2.extensions
+import tornado.ioloop
+import tornado.gen
 
 """
 Implements a wait function for Psycopg in order to wait for events while being
-nice with gevent (not blocking, etc.).
+nice with tornado (not blocking, etc.).
 
 See http://initd.org/psycopg/docs/advanced.html#asynchronous-support
+and https://gist.github.com/FSX/861193
 """
 
-def init_psycopg_gevent():
+ioloop = tornado.ioloop.IOLoop.instance()
+
+def init_psycopg_tornado():
+
+    @tornado.gen
     def wait(conn, timeout=None):
         while True:
             state = conn.poll()
             if state == psycopg2.extensions.POLL_OK:
                 break
             elif state == psycopg2.extensions.POLL_READ:
-                gevent.socket.wait_read(conn.fileno(), timeout=timeout)
+                ioloop.add_handler(conn.fileno(),
+                                   (yield gen.Callback('read_psycopg2')),
+                                   tornado.ioloop.IOLoop.READ)
+                yield tornado.gen.Wait('read_psycopg2')
             elif state == psycopg2.extensions.POLL_WRITE:
-                gevent.socket.wait_write(conn.fileno(), timeout=timeout)
+                ioloop.add_handler(conn.fileno(),
+                                   (yield gen.Callback('write_psycopg2')),
+                                   tornado.ioloop.IOLoop.WRITE)
+                yield tornado.gen.Wait('write_psycopg2')
             else:
                 raise psycopg2.OperationalError("invalid poll state")
 
