e18c40d3ed678f3648b4105b4c3e7fad2f58562d
Antoine Pietri <antoine.pietri@epita.fr>
2013-08-12 22:38:53 +0200

    get rid of gevent (finally), updated some licenses (s/stechec/prologin-sadm/)

--- python-lib/prologin/master-node/utils.py
+++ python-lib/prologin/master-node/utils.py
@@ -1,21 +1,22 @@
 # -*- encoding: utf-8 -*-
-# This file is part of Stechec.
+# This file is part of Prologin-SADM.
 #
+# Copyright (c) 2013 Antoine Pietri <antoine.pietri@prologin.org>
 # Copyright (c) 2011 Pierre Bourdon <pierre.bourdon@prologin.org>
 # Copyright (c) 2011 Association Prologin <info@prologin.org>
 #
-# Stechec is free software: you can redistribute it and/or modify
+# Prologin-SADM is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Stechec is distributed in the hope that it will be useful,
+# Prologin-SADM is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Stechec.  If not, see <http://www.gnu.org/licenses/>.
+# along with Prologin-SADM.  If not, see <http://www.gnu.org/licenses/>.
 
 import psycopg2
 import psycopg2.extensions
