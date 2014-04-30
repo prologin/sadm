23b86f2558c1b7b50208e850b6ddec37b759764b
Dettorer <p_hervot@hotmail.fr>
2014-04-30 15:52:52 +0200

    rfs: update install.py and the doc

--- rfs/init.sh
+++ rfs/init.sh
@@ -13,15 +13,16 @@ if [ x"$PACKAGES" == "x" ]; then
   exit 1
 fi
 
-pacman -Sy devtools nfs-utils openssh
+mkdir -p "$ROOTFS"
+pacman -Sy archlinux-install-scripts nfs-utils openssh
 for svc in {sshd,nfsd,rpc-{idmapd,gssd,mountd,statd}}.service; do
   systemctl enable "$svc"
   systemctl start  "$svc"
 done
-echo "$ROOTFS $SUBNET(ro,no_root_squash,subtree_check,async)" > /etc/exports.d/rootfs.exports
-mkarchroot "$ROOTFS" base $PACKAGES
-exportfs -arv
+pacstrap -d "$ROOTFS" base $PACKAGES
 cp -rv initcpio $ROOTFS/lib/
 cp rfs.sh "$ROOTFS/"
-mkarchroot -r "/rfs.sh" "$ROOTFS"
+arch-chroot "$ROOTFS" bash /rfs.sh
 rm -f "$ROOTFS/rfs.sh"
+echo "$ROOTFS $SUBNET(ro,no_root_squash,subtree_check,async)" > /etc/exports.d/rootfs.exports
+exportfs -arv
