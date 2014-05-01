abb327562d2f0b3f1cfe036d4d819ac2ac59ba2c
Dettorer <p_hervot@hotmail.fr>
2014-05-02 01:59:38 +0200

    rfs: setup the nfsroot's resolv.conf

--- rfs/rfs.sh
+++ rfs/rfs.sh
@@ -11,6 +11,7 @@ sed -e 's:^#en_US:en_US:g' -e 's:^#fr_FR:fr_FR:g' -i /etc/locale.gen
 sed -e 's:^HOOKS.*:HOOKS="base udev autodetect modconf net block filesystems keyboard fsck prologin":g' \
     -e 's:^MODULES.*:MODULES="nfsv3":g' -i /etc/mkinitcpio.conf
 sed -e 's:^CheckSpace:#CheckSpace:' -e 's:^SigLevel.*:SigLevel = Never:' -i /etc/pacman.conf
+sed -e 's:^#nameserver.*:nameserver 192.168.1.254:g' -i /etc/resolv.conf
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 echo KEYMAP=us > /etc/vconsole.conf
 locale-gen
