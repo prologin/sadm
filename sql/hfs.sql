ab8ded7d7887b7e55daf36d6ba893c2db139f4d3
Dettorer <p_hervot@hotmail.fr>
2014-05-04 04:16:58 +0200

    hfs: fix sql

--- sql/hfs.sql
+++ sql/hfs.sql
@@ -1,5 +1,6 @@
 CREATE USER hfs WITH PASSWORD 'DEFAULT_PASSWORD';
 CREATE DATABASE hfs OWNER hfs;
-\c hfs; -- Connect to database hfs
+-- Connect to database hfs
+\c hfs;
 SET ROLE hfs; -- Switch to user hfs
 CREATE TABLE user_location(id SERIAL, hfs INTEGER, username VARCHAR);
