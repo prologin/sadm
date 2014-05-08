dd2dd09938ee3128f48fa4383b9574bed00a2dcb
Alexandre Macabies <web@zopieux.com>
2014-05-08 19:12:36 +0000

    udbsync_redmine now syncs groups (roles have to be assigned manually through admin)

--- webservices/redmine/user_update.rb
+++ webservices/redmine/user_update.rb
@@ -8,40 +8,52 @@ users = conf['users']
 commands = conf['commands']
 
 commands.each do |login, cmd|
-	user = users[login]
+        user = users[login]
 
-	case cmd
-	when 'created'
-		# FIXME: use firstname/lastname instead
-		u = User.new(
-			:firstname => login,
-			:lastname => user['realname'],
-			:mail => login + '@finale.prologin'
-		)
-		u.login = login
-		u.password = user['password']
-		u.password_confirmation = user['password']
-		if u.valid?
-			u.save
-		else
-			$stderr.puts "invalid created user #{login}"
-		end
+        case cmd
+        when 'created'
+                # FIXME: use firstname/lastname instead
+                u = User.new(
+                        :firstname => login,
+                        :lastname => user['realname'],
+                        :mail => login + '@finale.prologin'
+                )
+                u.login = login
+                u.password = user['password']
+                u.password_confirmation = user['password']
+                if u.valid?
+                        u.save
+                else
+                        $stderr.puts "invalid created user #{login}"
+                end
 
-	when 'deleted'
-		u = User.find_by_login(login)
-		u.destroy
+        when 'deleted'
+                u = User.find_by_login(login)
+                u.destroy
 
-	when 'updated'
-		u = User.find_by_login(login)
-		u.password = user['password']
-		u.password_confirmation = user['password']
-		if u.valid? then
-			u.save
-		else
-			$stderr.puts "invalid updated user #{login}"
-		end
+        when 'updated'
+                u = User.find_by_login(login)
+                u.password = user['password']
+                u.password_confirmation = user['password']
+                if u.valid? then
+                        u.save
+                else
+                        $stderr.puts "invalid updated user #{login}"
+                end
+        end
 
-	end
+        if cmd == 'created' || cmd == 'updated'
+                u = User.find_by_login(login)
+                ['user', 'orga', 'root'].each do |gname|
+                        g = Group.where(lastname: gname).first_or_create
+                        g.save
+                        g.users.delete(u)
+                end
+
+                g = Group.where(lastname: user['group']).first
+                g.users << u
+        end
+
 end
 
 __END__
