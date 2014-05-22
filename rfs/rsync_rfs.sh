f041d4177c1a7efda85ddc71ec9317553c4bc7da
Rémi Audebert <halfr@halfr.net>
2014-05-22 14:26:21 +0200

    Add rfs syncing script

---
+++ rfs/rsync_rfs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+for serv in rfs{2,4,6}; do
+    echo "######## SYNCING $serv ########"
+    rsync -axPHAX /export/nfsroot/ $serv:/export/nfsroot
+    rsync -axPHAX /export/skeleton/ $serv:/export/skeleton
+done
