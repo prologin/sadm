18eb5dedeeae0dba294d74c1555eef4468e515f7
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2015-02-08 09:11:26 +0100

    rfs: fix scripts

--- rfs/rfs.sh
+++ rfs/rfs.sh
@@ -6,12 +6,12 @@
 set -e
 
 # Usual basic setup
-ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 sed -e 's:^#en_US:en_US:g' -e 's:^#fr_FR:fr_FR:g' -i /etc/locale.gen
 sed -e 's:^HOOKS.*:HOOKS="base udev autodetect modconf net block filesystems keyboard fsck prologin":g' \
     -e 's:^MODULES.*:MODULES="nfsv3":g' -i /etc/mkinitcpio.conf
 sed -e 's:^CheckSpace:#CheckSpace:' -e 's:^SigLevel.*:SigLevel = Never:' -i /etc/pacman.conf
-sed -e 's:^After=(.*):After=\1 set_hostname.service:' -i /usr/lib/systemd/system/collectd.service
+sed -e 's:^After=(.*):After=$1 set_hostname.service:' -i /usr/lib/systemd/system/collectd.service
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 echo KEYMAP=us > /etc/vconsole.conf
 locale-gen
