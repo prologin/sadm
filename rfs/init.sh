8f9daf6b3d2b1fa240229dada81e275399cd41c2
Rémi Audebert <halfr@halfr.net>
2015-03-29 16:31:37 +0200

    rfs: comment install scripts

--- rfs/init.sh
+++ rfs/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script setups the exported rootfs
+
 set -e
 
 if [ x"$ROOTFS" == "x" ]; then
@@ -17,47 +19,52 @@ fi
 
 mkdir -p "$ROOTFS"
 
-# Install the tools needed to install and serve the rfs
+echo 'Install the tools needed to install and serve the rfs'
 pacman -Sy --needed --noconfirm arch-install-scripts nfs-utils openssh dnsutils nbd
 
-# Install the base system (in rfs)
+echo 'Install the base system (in rfs)'
 pacstrap -d "$ROOTFS" base $PACKAGES
 
-# Copy some tools we will use in chroot
+echo 'Copy some tools we will use in chroot'
 cp -rv initcpio $ROOTFS/lib/    # initramfs hook
 cp -rv .. "$ROOTFS/sadm"        # sadm (we'll need some of it's services)
 cp rfs.sh "$ROOTFS/"            # the script executed by chroot below
 
-# Chroot to install the Arch Linux used by contestants
+echo 'Chroot to install the Arch Linux used by contestants'
 arch-chroot "$ROOTFS" bash /rfs.sh
 
-# Give the new system a nameserver (the gateway)
-sed -e 's:^#nameserver.*:domain prolo\nnameserver 192.168.1.254:g' -i /export/nfsroot/etc/resolv.conf
+echo 'Give the new system a nameserver (the gateway)'
+echo 'domain prolo
+nameserver 192.168.1.254' > /export/nfsroot/etc/resolv.conf
 
-# Load nbd driver at startup
+echo 'Load nbd driver at startup'
 echo nbd > /export/nfsroot/etc/modules-load.d/nbd.conf
 
-# Clean the rfs by removing our installation tools
+echo 'Clean the rfs by removing our installation tools'
 rm -f "$ROOTFS/rfs.sh"
 rm -rf "$ROOTFS/sadm"
 
-# Setup necessary kdm sessions
+echo 'Setup necessary kdm sessions'
 cd /export/nfsroot/usr/share/apps/kdm
 rm -rf sessions
 ln -s ../../xsessions sessions
 cd - # ~/sadm/rfs
 
-# Enable and start the services need to serve the rfs
 cd .. # ~/sadm
+echo 'Enable and start the services need to serve the rfs'
 python install.py udbsync_passwd udbsync_rfs
 for svc in {sshd,udbsync_passwd{,_nfsroot},rpcbind,nfs-server}.service; do
   systemctl enable "$svc"
   systemctl start  "$svc"
 done
 
-# And finally export the rfs via nfs
+echo 'And finally export the rfs via nfs'
 echo "$ROOTFS $SUBNET(ro,no_root_squash,subtree_check,async)" > /etc/exports.d/rootfs.exports
 exportfs -arv
+
 echo "--------------"
-echo "WARNING: Do not forget to copy the initrd and the kernel ($ROOTFS/boot/initramfs-linux.img and $ROOTFS/boot/vmlinuz-linux) to the directory specified in ./etc/prologin/netboot.yml (likely /srv/tftp/) and name them respectively 'initrd' and 'kernel'"
+echo "WARNING: Do not forget to copy the initrd and the kernel
+($ROOTFS/boot/initramfs-linux.img and $ROOTFS/boot/vmlinuz-linux) to the
+directory specified in ./etc/prologin/netboot.yml (likely /srv/tftp/) and name
+them respectively 'initrd' and 'kernel'"
 echo "--------------"
--- rfs/rfs.sh
+++ rfs/rfs.sh
@@ -7,27 +7,30 @@
 
 set -e
 
-# Usual basic setup
+echo 'Setting timezone'
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-
-sed -e 's:^#en_US:en_US:g' -e 's:^#fr_FR:fr_FR:g' -i /etc/locale.gen
-locale-gen
-
-sed -e 's:^HOOKS.*:HOOKS="base udev autodetect modconf net block filesystems keyboard fsck prologin":g' \
-    -e 's:^MODULES.*:MODULES="nfsv3":g' -i /etc/mkinitcpio.conf
-sed -e 's:^CheckSpace:#CheckSpace:' -e 's:^SigLevel.*:SigLevel = Never:' -i /etc/pacman.conf
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-echo KEYMAP=us > /etc/vconsole.conf
-
 echo '[Time]
 NTP=ntp.prolo' > /etc/systemd/timesyncd.conf
 
+echo 'Setting locales'
+sed -e 's:^#en_US:en_US:g' -e 's:^#fr_FR:fr_FR:g' -i /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo KEYMAP=us > /etc/vconsole.conf
+
+echo 'Setting pacman'
+sed -e 's:^CheckSpace:#CheckSpace:' -e 's:^SigLevel.*:SigLevel = Never:' -i /etc/pacman.conf
+
+echo 'Generating host ssh keys'
 ssh-keygen -A
 
-# Regenerate an initramfs in order to include the prologin hook
+echo 'Adding initrd hooks and modules'
+sed -e 's:^HOOKS.*:HOOKS="base udev autodetect modconf net block filesystems keyboard fsck prologin":g' \
+    -e 's:^MODULES.*:MODULES="nfsv3":g' -i /etc/mkinitcpio.conf
+echo 'Regenerating an initramfs in order to include the prologin hook'
 mkinitcpio -p linux
 
-# Install the prologin virtualenv and library
+echo 'Install the prologin virtualenv and library'
 mkdir /var/prologin
 virtualenv3 /var/prologin/venv
 source /var/prologin/venv/bin/activate
@@ -37,5 +40,5 @@ python install.py libprologin
 # And some sadm services
 python install.py presenced set_hostname
 
-# Enable some services
+echo 'Enable some services'
 systemctl enable sshd presenced set_hostname kdm
