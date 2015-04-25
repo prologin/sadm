77ec54d5cccbe140ba0212b3ef75951a2d8ec02c
Alexandre `Zopieux` Macabies <web+git@zopieux.com>
2015-04-25 14:45:56 +0200

    sql: post-review fix: remove default modifiers in CREATE ROLE

--- sql/redmine.sql
+++ sql/redmine.sql
@@ -1,4 +1,4 @@
-CREATE ROLE redmine WITH LOGIN ENCRYPTED PASSWORD 'DEFAULT_PASSWORD' NOINHERIT VALID UNTIL 'infinity';
+CREATE ROLE redmine WITH LOGIN ENCRYPTED PASSWORD 'DEFAULT_PASSWORD';
 CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER redmine;
 -- See http://www.redmine.org/projects/redmine/wiki/redmineinstall#Supported-database-back-ends
 ALTER DATABASE redmine SET datestyle="ISO,MDY";
