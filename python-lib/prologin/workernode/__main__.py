f1eb34c2a4f0ebae58745baafe5abf5056c547ac
Rémi Audebert <halfr@halfr.net>
2015-04-26 18:28:03 +0200

    workernode: add missing import

--- python-lib/prologin/workernode/__main__.py
+++ python-lib/prologin/workernode/__main__.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Prologin-SADM.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 import optparse
 import prologin.config
 import prologin.log
