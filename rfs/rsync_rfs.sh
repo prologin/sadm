e2d496b44828ad624c5a76d9a3b1140fb764467e
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2015-05-07 22:50:17 +0200

    rsync_rfs: --delete to remove files

--- rfs/rsync_rfs.sh
+++ rfs/rsync_rfs.sh
@@ -4,6 +4,6 @@ set -e
 
 for serv in rfs{2,4,6}; do
     echo "######## SYNCING $serv ########"
-    rsync -axPHAX /export/nfsroot/ "$serv":/export/nfsroot
-    rsync -axPHAX /export/skeleton/ "$serv":/export/skeleton
+    rsync --delete -axPHAX /export/nfsroot/ "$serv":/export/nfsroot
+    rsync --delete -axPHAX /export/skeleton/ "$serv":/export/skeleton
 done
