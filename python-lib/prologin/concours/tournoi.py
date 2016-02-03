7c95b46d83f8285f1ae6675d0aa117fb8fc67e1f
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2016-02-03 15:58:07 +0000

    tournoi: update stub scripts

--- python-lib/prologin/concours/tournoi.py
+++ python-lib/prologin/concours/tournoi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 
 import django
 import os
@@ -13,80 +13,36 @@ from prologin.concours.stechec.models import Tournament, Match, MatchPlayer, Cha
 
 prologin = User.objects.get(username="seirl")
 tournoi = Tournament.objects.create()
-maptournoi = Map.objects.get(pk=4)
+
+print('Launching tournament {}'.format(tournoi.id))
 
 def lancer_match(c1, c2):
     m = Match(author=prologin,
               tournament=tournoi)
-    m.map = maptournoi.path
-    MatchPlayer(champion=c1, match=m).save()
-    MatchPlayer(champion=c2, match=m).save()
     m.status = 'new'
     m.save()
+    MatchPlayer(champion=c1, match=m).save()
+    MatchPlayer(champion=c2, match=m).save()
     return m.id
 
-def all_finished(matches):
-    for id in matches:
-        m = Match.objects.get(pk=id)
-        if m.status != 'done':
-            return False
-    return True
-
-print('Fetching all competitors')
 chs = []
 for u in User.objects.all():
-    ch = Champion.objects.filter(author=u, deleted=False).order_by('-id')
+    ch = Champion.objects.filter(author=u, author__is_staff=False,
+                                 deleted=False).order_by('-id')
     if len(ch) > 0:
         chs.append(ch[0])
 
-print('Done. Champions are :')
+print()
+print('Champions are :')
 for ch in chs:
     print('-', ch)
 
+print()
 print('Launching matches...')
-matches = []
 for c1 in chs:
     for c2 in chs:
-        if c1.id == c2.id:
-            continue
-        print(c1.id, '-', c2.id)
-        matches.append(lancer_match(c1, c2))
-
-print('Waiting for matches to end...')
-while not all_finished(matches):
-    time.sleep(5)
-    print('...')
-
-print('Getting the matches...')
-matches = [Match.objects.get(pk=id) for id in matches]
-
-#matches = list(Match.objects.filter(createur = prologin, id__gt = 37911))
-
-print('Calculating the scores...')
-score = {}
-indice = {}
-for m in matches:
-    c1, c2 = tuple(MatchPlayer.objects.filter(match=m))
-    indice[c1.champion.id] = indice.get(c1.champion.id, 0) + c1.score
-    indice[c2.champion.id] = indice.get(c2.champion.id, 0) + c2.score
-    if c1.score > c2.score:
-        score[c1.champion.id] = score.get(c1.champion.id, 0) + 2
-    elif c2.score > c1.score:
-        score[c2.champion.id] = score.get(c2.champion.id, 0) + 2
-    else:
-        score[c1.champion.id] = score.get(c1.champion.id, 0) + 1
-        score[c2.champion.id] = score.get(c2.champion.id, 0) + 1
-
-print('Sorting...')
-l = [(score.get(ch.id, 0), indice.get(ch.id, 0), ch.id) for ch in chs]
-l.sort()
-
-print('Saving...')
-for (score, indice, id) in l:
-    ch = Champion.objects.get(pk=id)
-    p = TournamentPlayer(
-        champion = ch,
-        tournament = tournoi,
-        score = score
-    )
-    p.save()
+        for i in range(20):
+            if c1.id == c2.id:
+                continue
+            print(c1.id, '-', c2.id)
+            lancer_match(c1, c2)
