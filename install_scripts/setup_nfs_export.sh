5787bbdd8065323b98e4934d976ef3ad659edbd8
Rémi Audebert <rflah0@gmail.com>
2017-04-14 19:05:27 +0200

    install_scripts: add nfs export setup script

--- install_scripts/setup_nfs_export.sh
+++ install_scripts/setup_nfs_export.sh
@@ -5,33 +5,30 @@
 
 set -e
 
-echo 'Setting systemd-logind'
+echo '[+] Configure systemd-logind'
 sed -e 's:^#KillUserProcesses=no:KillUserProcesses=yes:' \
     -e 's:^#KillExcludeUsers=root:KillExcludeUsers=root:' -i /etc/systemd/logind.conf
 
-echo 'Setting pacman'
-sed -e 's:^CheckSpace:#CheckSpace:' -e 's:^SigLevel.*:SigLevel = Never:' -i /etc/pacman.conf
-
-echo 'Generating host ssh keys'
+echo '[+] Generate host ssh keys'
 ssh-keygen -A
 
-echo 'Adding initrd hooks and modules'
+echo '[+] Add initrd hooks and modules'
 sed -e 's:^HOOKS.*:HOOKS="base udev autodetect modconf block filesystems keyboard fsck prologin":g' \
     -e 's:^MODULES.*:MODULES="nfsv3":g' -i /etc/mkinitcpio.conf
-echo 'Regenerating an initramfs in order to include the prologin hook'
+
+echo '[+] Regenerate an initramfs with SADM configuration'
 mkinitcpio -p linux
 
-echo 'Load nbd driver at startup'
+echo '[+] Load nbd driver at startup'
 echo nbd > /etc/modules-load.d/nbd.conf
 
-echo 'Install the prologin virtualenv and library'
+echo '[+] Configure the system for SADM (setup_sadm.sh)'
 ./setup_sadm.sh
 
 source /var/prologin/venv/bin/activate
 python install.py libprologin
 python install.py presenced
+python install.py sddmcfg
 
 echo 'Enable some services'
 systemctl enable presenced sddm
-
-echo 'rfs: all done!'
