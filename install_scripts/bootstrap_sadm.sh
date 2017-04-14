835aba154aa933cba7db8e6478005abfb5be1f78
Rémi Audebert <rflah0@gmail.com>
2017-04-14 19:02:17 +0200

    install_scripts: bootstrap_sadm.sh: use this script for bare metal install

--- install_scripts/bootstrap_sadm.sh
+++ install_scripts/bootstrap_sadm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# This script does a standalone clone and setup of sadm from the main git
-# repository. If you are developping on SADM, directly use
-# install_scripts/setup_sadm.sh
+# Run this script if you are installing from an Arch Linux install media.
+# It does a standalone clone and setup of sadm from the main git repository. If
+# you are developping on SADM, directly use install_scripts/setup_sadm.sh
 
 if mount | grep --quiet archiso; then
   # When running on the Arch Linux install media, increase the overlay work dir
@@ -16,8 +16,10 @@ pacman -S --needed --noconfirm git
 
 echo '[+] Cloning sadm'
 cd /root
-git clone https://github.com/prologin/sadm.git
+git clone https://github.com/prologin/sadm
 
-echo '[+] Starting sadm setup script'
-cd sadm
-./install_scripts/setup_sadm.sh
+echo '[+] Starting Arch Linux bootstrap script'
+./sadm/install_scripts/bootstrap.sh
+
+echo '[+] Copying SADM install'
+cp -r sadm /mnt/root/sadm
