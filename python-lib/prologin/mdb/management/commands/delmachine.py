0899117a580811c61b600850a5707fb061096ba8
Rémi Audebert <halfr@lse.epita.fr>
2017-04-25 07:29:39 +0000

    mdb: add management command: delmachine

---
+++ python-lib/prologin/mdb/management/commands/delmachine.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from prologin.mdb.models import Machine
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('hostname')
+
+    def handle(self, *args, **options):
+        Machine.objects.get(hostname=options['hostname']).delete()
