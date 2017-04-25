ffe01809ac514df2e24c40035d6ddee97bdd3fb8
Rémi Audebert <halfr@lse.epita.fr>
2017-04-25 07:32:41 +0000

    checks: refactor

---
+++ checks/check_rhfs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script performs various system sanity checks for rhfsAB.prolo
+
+cd $(dirname $0)
+
+source ./checks_common.sh
+
+function main {
+  echo_status "systemd units"
+  for svc in {udbsync_passwd{,_nfsroot},udbsync_rootssh,rpcbind,nfs-server}.service rootssh.path; do
+    test_service_is_enabled_active "$svc"
+  done
+
+
+  echo_status "network"
+  not_in_alien_subnet
+}
+
+main
