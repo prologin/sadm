49f593ac64ae1fdc1cf16104227974981ede9a18
Rémi Audebert <halfr@lse.epita.fr>
2017-04-25 07:32:41 +0000

    checks: refactor

--- checks/check_gw.sh
+++ checks/check_gw.sh
@@ -1,55 +1,19 @@
 #!/bin/bash
 # This script performs various system sanity checks for gw.prolo
 
-# Logging functions
-function echo_status {
- # blue foreground, then default foreground
-  echo -e "\e[34m[+] $*\e[39m"
-}
+cd $(dirname $0)
 
-function echo_ko {
-  # red foreground, then default foreground
-  echo -e "\e[31m$*\e[39m"
-}
-
-function echo_ok {
-  # green foreground, then default foreground
-  echo -e "\e[32m$*\e[39m"
-}
-
-function test_service_is_enabled_active {
-  service=$1
-  service_status=$(systemctl is-active $service)
-
-  echo -n "$service is $service_status "
-  if [[ $service_status == active ]]; then
-    echo_ok "OK"
-  else
-    echo_ko "FAIL: should be active"
-  fi
-
-  service_status=$(systemctl is-enabled $service)
-  echo -n "$service is $service_status "
-  if [[ $service_status == enabled ]]; then
-    echo_ok "OK"
-  else
-    echo_ko "FAIL: should be enabled"
-  fi
-}
-
-function test_file_present {
-  filename=$1
-  echo -n "$filename is "
-  if [[ -e $filename ]]; then
-    echo_ok "present"
-  else
-    echo_ko "absent"
-  fi
-}
+source ./checks_common.sh
 
 function main {
-  echo_status "systemd units"
+  echo_status "hostname"
+  check_hostname gw
 
+  echo_status "network"
+  check_ip 192.168.1.254
+  check_ip 192.168.250.254
+
+  echo_status "systemd units"
   for service in systemd-networkd.service \
     postgresql.service \
     mdb.service \
@@ -74,7 +38,10 @@ function main {
   done
 
   echo_status "files"
+  test_file_present /srv/tftp/arch.kpxe
   test_file_present /srv/tftp/prologin.kpxe
+  test_file_present /srv/tftp/initrd
+  test_file_present /srv/tftp/vmlinuz
 }
 
 # Run all the checks
