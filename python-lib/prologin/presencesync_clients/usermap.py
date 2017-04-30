48a0e0dc0452653a4a6afaf6d21d75774860afba
Antoine Pietri <antoine.pietri@prologin.org>
2017-04-30 02:54:30 +0000

    usermap: add colors for unregistered, faulty and off

--- python-lib/prologin/presencesync_clients/usermap.py
+++ python-lib/prologin/presencesync_clients/usermap.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 
+import asyncio
 import logging
 import prologin.config
 import prologin.log
+import prologin.mdbsync.client
 import prologin.presencesync.client
-import prologin.udb.client
+import prologin.udbsync.client
+import subprocess
 import xml.etree.ElementTree as ET
 
 """Generate a map of connected contestants
@@ -28,6 +31,8 @@ example.
 CFG = prologin.config.load('presencesync_usermap')
 
 
+G_TAG = '{http://www.w3.org/2000/svg}g'
+RECT_TAG = '{http://www.w3.org/2000/svg}rect'
 TEXT_TAG = '{http://www.w3.org/2000/svg}text'
 TSPAN_TAG = '{http://www.w3.org/2000/svg}tspan'
 
@@ -50,8 +55,27 @@ STYLES = {
     'disconnected': (
         'font-weight: bold;',
         'fill: #b0b0b0; font-style: italic;'
-    )
+    ),
 }
+
+
+def fill_rect(rect, status=True, registered=False, faulty=False):
+    """
+    Fill the rectangle of the machine in the usermap.
+    """
+
+    style = rect.get('style', '')
+
+    if not registered:
+        rect.set('fill', '#a9d0f5')
+        rect.set('style', '')
+        rect.set('stroke', '#2e64fe')
+    elif faulty:
+        rect.set('fill', 'url(#faultystripe)')
+        rect.set('stroke', '#a6a6a6')
+        rect.set('style', '')
+    elif not status:
+        rect.set('style', rect.get('style', '') + ';' + 'fill: #ff0000')
 
 
 def fill_machine(text, login=None, group=""):
@@ -71,12 +95,27 @@ def fill_machine(text, login=None, group=""):
         tspan.set('style', style)
 
 
-def generate(host_to_login, udb_users, map_pattern, output):
+mdb_machines = {}
+udb_users = {}
+presence_data = {}
+ping_status = {}
+
+
+def generate(map_pattern, output):
     """Write the SVG user map into the `output` using the `map_pattern`
     readable file and the `logins` -> hostname mapping.
     """
+    host_to_login = {
+        entry['hostname']: entry['login']
+        for entry in presence_data.values()
+    }
+
     tree = ET.parse(map_pattern)
-    for text in tree.getroot().iter(TEXT_TAG):
+    for g in tree.getroot().iter(G_TAG):
+        if not len(g) or g[0].tag != RECT_TAG or g[1].tag != TEXT_TAG:
+            continue
+        rect = g[0]
+        text = g[1]
         if (
             len(text) == 2 and
             text[0].tag == TSPAN_TAG and
@@ -87,37 +126,95 @@ def generate(host_to_login, udb_users, map_pattern, output):
 
             group = "user"  # default group
             # Search user group
-            for udb_user in udb_users:
+            for udb_user in udb_users.values():
                 if udb_user['login'] == login:
                     group = udb_user['group']
                     break
 
+            status = ping_status.get(machine_name, True)
+
+            faulty = False
+            registered = False
+            for machine in mdb_machines.values():
+                if machine['hostname'] == machine_name:
+                    faulty = machine['is_faulty']
+                    registered = True
+                    break
+
             fill_machine(text, login, group)
+            fill_rect(rect, status, registered, faulty)
 
     tree.write(output, encoding='utf-8', xml_declaration=True)
 
-def callback(logins, updates_metadata):
-    host_to_login = {
-        entry['hostname']: entry['login']
-        for entry in logins.values()
-    }
+
+def update_map():
     logging.info('Upgrade using updates')
-    udb_users = prologin.udb.client.connect().query()  # get all users
     try:
         with open(CFG['map_pattern'], 'rb') as map_pattern:
             with open(CFG['output'], 'wb') as output:
-                generate(host_to_login, udb_users, map_pattern, output)
+                generate(map_pattern, output)
     except IOError as e:
         logging.exception('Cannot open files')
     except ET.ParseError as e:
         logging.exception('Cannot parse the map pattern')
 
 
+async def ping_machine(hostname):
+    proc = await asyncio.create_subprocess_exec(
+            'ping', '-c', '1', '-W', '2', hostname,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+    return hostname, (await proc.wait() == 0)
+
+
+async def update_ping(loop):
+    while True:
+        await asyncio.sleep(5)
+
+        try:
+            tasks = [ping_machine(m['hostname']) for m in mdb_machines.values()]
+            if not tasks:
+                continue
+
+            done, _ = await asyncio.wait(tasks)
+
+            new_ping_status = {}
+            for future in done:
+                hostname, status = future.result()
+                new_ping_status[hostname] = status
+
+            if new_ping_status != ping_status:
+                ping_status.clear()
+                ping_status.update(new_ping_status)
+                loop.call_soon_threadsafe(update_map)
+        except:
+            logging.exception('An error while pinging the machines')
+
+
+async def poll_all():
+    udbsync_client = prologin.udbsync.client.connect()
+    mdbsync_client = prologin.mdbsync.client.connect()
+    presencesync_client = prologin.presencesync.client.connect()
+
+    loop = asyncio.get_event_loop()
+
+    tasks = []
+
+    def add_task(client, dict_to_update):
+        def cb(values, meta):
+            dict_to_update.clear()
+            dict_to_update.update(values)
+            loop.call_soon_threadsafe(update_map)
+        tasks.append(loop.run_in_executor(None, client.poll_updates, cb))
+
+    add_task(mdbsync_client, mdb_machines)
+    add_task(udbsync_client, udb_users)
+    add_task(presencesync_client, presence_data)
+    tasks.append(update_ping(loop))
+
+    await asyncio.wait(tasks)
+
+
 if __name__ == '__main__':
     prologin.log.setup_logging('presencesync_usermap')
-    prologin.presencesync.client.connect().poll_updates(callback)
-    args = parser.parse_args()
-    try:
-        generate(args)
-    except ET.ParseError as e:
-        parser.error(str(e))
+    asyncio.get_event_loop().run_until_complete(poll_all())
