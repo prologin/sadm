58ed3612e8a021459b39af793e3be22606c8aac8
Antoine Pietri <antoine.pietri1@gmail.com>
2018-03-10 20:32:21 +0100

    language docs: java: update to JDK9 url

--- webservices/docs/languages/get_docs.sh
+++ webservices/docs/languages/get_docs.sh
@@ -15,7 +15,7 @@ wget -c -nv http://caml.inria.fr/distrib/ocaml-${OCAML_VERSION}/ocaml-${OCAML_VE
 wget -c -nv https://docs.python.org/3/archives/python-${PYTHON_VERSION}-docs-html.tar.bz2 -O python.tar.bz2
 wget -c -nv http://fr.php.net/get/php_manual_fr.tar.gz/from/this/mirror -O php.tar.gz
 wget -c -nv https://downloads.haskell.org/~ghc/latest/docs/libraries.html.tar.xz -O haskell.tar.gz
-wget -c -nv --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-docs-all.zip -O java.zip
+wget -c -nv --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_doc-all.zip" -O java.zip
 
 # C
 if [ -f c.tar.gz ]; then
