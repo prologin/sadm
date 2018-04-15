ba23bb01b2a8379ce26ad453aeb92aee81b146af
Rémi Audebert <rflah0@gmail.com>
2018-04-15 15:26:40 +0200

    containers: add container_setup_pas-r11p11.sh
    Container script for testing the exported RFS system.

---
+++ install_scripts/container_setup_pas-r11p11.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Configuration variables
+CONTAINER_NAME=pas-r11p11
+CONTAINER_HOSTNAME=$CONTAINER_NAME
+CONTAINER_MAIN_IP=192.168.1.111
+MDB_MACHINE_TYPE=user
+
+GW_CONTAINER_NAME=mygw
+RHFS_CONTAINER_NAME=myrhfs0
+
+source ./container_setup_common.sh
+
+container_script_header
+
+function stage_setup_rootfs {
+  echo_status 'Fake NFS-root using mount -o bind'
+
+  mkdir -p $CONTAINER_ROOT
+  if ! findmnt $CONTAINER_ROOT; then
+    mount -o bind,ro /var/lib/machines/$RHFS_CONTAINER_NAME/export/nfsroot \
+       $CONTAINER_ROOT
+  fi
+}
+
+function stage_setup_network {
+  echo_status 'Fake PXE network configuration'
+
+  container_run /usr/bin/ip address replace $CONTAINER_MAIN_IP/23 dev host0
+  container_run /usr/bin/ip route replace default via 192.168.1.254
+}
+
+# Container script
+
+run container_stop
+run stage_setup_rootfs
+run container_start
+
+run stage_add_to_mdb
+
+run stage_setup_network
+run test_network
