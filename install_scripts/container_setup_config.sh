0dd6a45f7eaf4c828bd611797f7007fd87376d70
Rémi Audebert <rflah0@gmail.com>
2018-04-15 15:24:44 +0200

    containers: set default MDB_ALIASES to CONTAINER_HOSTNAME

--- install_scripts/container_setup_config.sh
+++ install_scripts/container_setup_config.sh
@@ -1,18 +1,17 @@
-# General
+# Host
 USE_BTRFS=true
+SADM_ROOT_DIR=$(dirname $PWD)
 
-# Container
+# Container network
 NETWORK_ZONE=prolo
 
 # SADM secrets
 ROOT_PASSWORD=101010
 SADM_MASTER_SECRET=lesgerbillescesttropfantastique
 
-# Misc
-SADM_ROOT_DIR=$(dirname $PWD)
-
-# MDB registration default values
+# SADM MDB registration default values
 MDB_ROOM=pasteur
 MDB_MACHINE_TYPE=service
 MDB_RFS_ID=0
 MDB_HFS_ID=0
+MDB_ALIASES=${MDB_ALIASES:-$CONTAINER_HOSTNAME}
