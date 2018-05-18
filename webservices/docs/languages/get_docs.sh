60192cdba4b5497fd91aead2bd2cdec2a006189f
Antoine Pietri <antoine.pietri1@gmail.com>
2018-05-18 20:02:46 +0200

    language docs: add rust, fix java path

--- webservices/docs/languages/get_docs.sh
+++ webservices/docs/languages/get_docs.sh
@@ -9,13 +9,14 @@ PYTHON_VERSION=$( python3 --version | cut -d' ' -f2)
 OCAML_VERSION=$( ocaml -vnum | cut -f1-2 -d. )
 
 # Download doc
-wget -c -nv http://www.acm.uiuc.edu/webmonkeys/book/c_guide.tar.gz -O c.tar.gz
-wget -c -nv http://upload.cppreference.com/mwiki/images/6/6c/html_book_20141118.tar.gz -O cpp.tar.gz
-wget -c -nv http://caml.inria.fr/distrib/ocaml-${OCAML_VERSION}/ocaml-${OCAML_VERSION}-refman-html.tar.gz -O ocaml.tar.gz
-wget -c -nv https://docs.python.org/3/archives/python-${PYTHON_VERSION}-docs-html.tar.bz2 -O python.tar.bz2
-wget -c -nv http://fr.php.net/get/php_manual_fr.tar.gz/from/this/mirror -O php.tar.gz
-wget -c -nv https://downloads.haskell.org/~ghc/latest/docs/libraries.html.tar.xz -O haskell.tar.gz
-wget -c -nv --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_doc-all.zip" -O java.zip
+wget -c http://www.acm.uiuc.edu/webmonkeys/book/c_guide.tar.gz -O c.tar.gz
+wget -c http://upload.cppreference.com/mwiki/images/6/6c/html_book_20141118.tar.gz -O cpp.tar.gz
+wget -c http://caml.inria.fr/distrib/ocaml-${OCAML_VERSION}/ocaml-${OCAML_VERSION}-refman-html.tar.gz -O ocaml.tar.gz
+wget -c https://docs.python.org/3/archives/python-${PYTHON_VERSION}-docs-html.tar.bz2 -O python.tar.bz2
+wget -c http://fr.php.net/get/php_manual_fr.tar.gz/from/this/mirror -O php.tar.gz
+wget -c https://downloads.haskell.org/~ghc/latest/docs/libraries.html.tar.xz -O haskell.tar.gz
+wget -c https://static.rust-lang.org/dist/rust-docs-nightly-x86_64-unknown-linux-gnu.tar.gz -O rust.tar.gz
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jdk-10.0.1_doc-all.zip" -O java.zip
 
 # C
 if [ -f c.tar.gz ]; then
@@ -75,10 +76,21 @@ fi
 
 # Haskell
 if [ -f haskell.tar.gz ]; then
+    echo "Installing Haskell doc"
     tar xf haskell.tar.gz
     mv libraries haskell
 else
     echo "** No Haskell doc installed"
+fi
+
+# Rust
+if [ -f rust.tar.gz ]; then
+    echo "Installing Rust doc"
+    tar xf rust.tar.gz
+    mv rust-docs-nightly-*/rust-docs/share/doc/rust/html/ rust
+    rm -rf rust-docs-nightly-*
+else
+    echo "** No Rust doc installed"
 fi
 
 # Clean
