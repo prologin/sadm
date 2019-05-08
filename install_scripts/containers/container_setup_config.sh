489032e8fd412c4d303084834e63db01b62b1085
Antoine Pietri <antoine.pietri1@gmail.com>
2019-05-08 23:24:54 +0200

    containers: add a gitignored config file

--- install_scripts/containers/container_setup_config.sh
+++ install_scripts/containers/container_setup_config.sh
@@ -1,4 +1,9 @@
-USE_BTRFS=true
+config_file="$( dirname $0 )/container_setup.conf"
+if [ -f "$config_file" ]; then
+    source "$config_file";
+fi
+
+USE_BTRFS=${USE_BTRFS:-true}
 DEBUG=${DEBUG:-true}
 
 # Container network name
