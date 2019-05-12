70d28ff303b0d32e3d8f1172f73467d8376422df
Antoine Pietri <antoine.pietri1@gmail.com>
2019-05-12 20:26:58 +0200

    concours: add launch_roundrobin command

---
+++ python-lib/prologin/concours/stechec/management/commands/launch_roundrobin.py
@@ -0,0 +1,51 @@
+import itertools
+import sys
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from prologin.concours.stechec.models import Match, Tournament
+
+
+class Command(BaseCommand):
+    help = "Launch a tournament in round-robin mode."
+
+    def add_arguments(self, parser):
+        parser.add_argument('tournament_id', type=int)
+
+    def handle(self, *args, **options):
+        self.launch(options['tournament_id'])
+
+    def gen_matches(self, tournament):
+        if settings.STECHEC_USE_MAPS and not tournament.maps.all():
+            raise RuntimeError("Configured to run with maps but no maps "
+                               "present in tournament.")
+
+        matches = []
+        print(list(itertools.product(tournament.players.all(),
+                                     repeat=settings.STECHEC_NPLAYERS)))
+        for chs in itertools.product(tournament.players.all(),
+                                     repeat=settings.STECHEC_NPLAYERS):
+            # Don't fight against yourself
+            ch_ids = [c.id for c in chs]
+            if len(set(ch_ids)) != len(ch_ids):
+                continue
+
+            match = {'author': tournament.author,
+                     'tournament': tournament,
+                     'champions': chs}
+
+            if settings.STECHEC_USE_MAPS:
+                for map in tournament.maps:
+                    matches.append({**match, 'map': map})
+            else:
+                matches.append(match)
+
+        return matches
+
+    def launch(self, tournament_id):
+        try:
+            tournament = Tournament.objects.get(id=tournament_id)
+        except Tournament.DoesNotExist:
+            sys.exit("Tournament {} does not exist.".format(tournament_id))
+        matches = self.gen_matches(tournament)
+        Match.launch_bulk(matches)
