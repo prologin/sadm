6291d75eabbd59313cb3c0bd0af29e577254ce58
Antoine Pietri <antoine.pietri1@gmail.com>
2019-05-12 20:32:12 +0200

    concours: admin: set current user as tournament author

--- python-lib/prologin/concours/stechec/admin.py
+++ python-lib/prologin/concours/stechec/admin.py
@@ -85,6 +85,10 @@ class TournamentAdmin(admin.ModelAdmin):
         defaults.update(kwargs)
         return super().get_form(request, obj, **defaults)
 
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
     def save_related(self, request, form, formsets, change):
         super().save_related(request, form, formsets, change)
 
