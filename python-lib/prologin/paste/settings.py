7f8214e275cd69d5ced57f8bc844f8bd6f541aa2
Antoine Pietri <antoine.pietri1@gmail.com>
2020-05-07 20:23:54 +0200

    paste, wiki: add missing SSO middleware

--- python-lib/prologin/paste/settings.py
+++ python-lib/prologin/paste/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'prologin.sso.django.SSOMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
